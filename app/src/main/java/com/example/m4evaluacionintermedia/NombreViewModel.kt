8da1c49db03148a7ac7f000ae673f8c5489c1b9f
copito.123 <nicolspnicolsp@gmail.com>
2020-09-10 23:21:22 -0300

    HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA NOMEGUNCIONA EL DIBUJO

--- app/src/main/java/com/example/m4evaluacionintermedia/NombreViewModel.kt
+++ app/src/main/java/com/example/m4evaluacionintermedia/NombreViewModel.kt
@@ -2,6 +2,38 @@ package com.example.m4evaluacionintermedia
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.m4evaluacionintermedia.DataBase.Nombre
+import com.example.m4evaluacionintermedia.DataBase.NombreDao
+import com.example.m4evaluacionintermedia.DataBase.NombreDataBase
+import kotlinx.coroutines.launch
 
 class NombreViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: NombreRepository
+    val allNombre: LiveData<List<Nombre>>
+
+
+    init {
+        val NombreDao = NombreDataBase.getDataBase(application).getNombreDao()
+        repository = NombreRepossitory(NombreDao)
+        allNombre = repository.listAllNombre
+    }
+    fun insertNombre(nombre: Nombre) = viewModelScope.launch {
+        repository.insertNombre(nombre)
+    }
+
+    fun deleteAllNombre() = viewModelScope.launch {
+        repository.deleteAll
+    }
+
+    fun getOneNombreByID(id : Int) : LiveData<Nombre> {
+        return repository.getOneNombreByID(id)
+    }
+
+    fun updateNombre(mNombre: Nombre) = viewModelScope.launch {
+        repository.updateNombre(mNombre)
+    }
+
 }
