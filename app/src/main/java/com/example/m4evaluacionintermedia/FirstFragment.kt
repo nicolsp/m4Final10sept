bd1733165c0073eac11c153acf160f4196bb8853
copito.123 <nicolspnicolsp@gmail.com>
2020-09-13 18:00:24 -0300

    YA NO FALTA TANTOOOOOOOOO
    correcion 13 09

--- app/src/main/java/com/example/m4evaluacionintermedia/FirstFragment.kt
+++ app/src/main/java/com/example/m4evaluacionintermedia/FirstFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.m4evaluacionintermedia.DataBase.Nombre
 import kotlinx.android.synthetic.main.fragment_first.*
 
@@ -18,19 +22,17 @@ import kotlinx.android.synthetic.main.fragment_first.*
 class FirstFragment : Fragment() {
 
     lateinit var mViewModel: NombreViewModel
-    private  var idNombre: Int? = null
-
+    private var idNombre: Int? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mViewModel = ViewModelProvider(this).get(NombreViewModel::class.java)
-            arguments?.let {
+        arguments?.let {
             idNombre = it.getInt("id")
             Log.d("OBJ", idNombre.toString())
         }
     }
-
 
 
     override fun onCreateView(
@@ -44,35 +46,52 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-      //  idNombre?.let {
-           // mViewModel.getOneNombreByID(it).Observe(viewLifecycleOwner, Observer, {
-             //   Log.d("OBJ_LIV",it.nombre)
-          //tv2.setText(it.nombre)
-            // AQUI VA LO QUE SE VE EN LAS VISTAS
-        //}
-        //}
+        idNombre?.let {
+            mViewModel.run {
+                getOneNombreByID(it).observe(viewLifecycleOwner, Observer {
+                    Log.d("OBJ_LIV", it.nombre)
+                    tv1.setText(it.nombre)
+                    tv3.setText(it.cantidad)
+                    cb.isChecked = it.completeNombre
 
-        mViewModel.allNombre.observe(viewLifecycleOwner, Observer {
-            Log.d("OBJ",it.toString())
-        })
+                })
+            }
 
-        button.setOnClickListener {
-            val textNombre = tv1.text.toString()
-            val precio = tv3.text.toString().toInt()
-            val cantidad = tv4.text.toString().toDouble()
-            val checkBox = cb.isChecked
-            val nombre = Nombre(0,textNombre,true,precio,cantidad)
 
-           // Toast.makeText(context,nombre.toString(),Toast.LENGTH_SHORT).show()
+            button.setOnClickListener {
+                val textNombre = tv1.text.toString()
+                val precio = tv3.text.toString().toInt()
+                val cantidad = tv4.text.toString().toDouble()
+                val checkBox = cb.isChecked
+                val nombre = Nombre(0, textNombre, true, precio, cantidad)
 
-            mViewModel.insertNombre(nombre)
+
+                if (textNombre.isNotEmpty()) {
+                    if (idNombre != null) {
+                        Log.d("OBJ_ID_TASK", idNombre.toString())
+                        val mNombre = Nombre(
+                            nombre = textNombre,
+                            completeNombre = checkBox!!
+                        )
+                        mViewModel.updateNombre(mNombre)
+                    } else {
+                        val mNombre = Nombre(
+                            nombre = textNombre,
+                            completeNombre = checkBox
+                        )
+                        mViewModel.insertNombre(mNombre)
+                    }
+                } else {
+                    Toast.makeText(context, "Debe llenar todos los espacios", Toast.LENGTH_LONG)
+                        .show()
+                }
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+            }
 
         }
-
-        //view.findViewById<Button>(R.id.button).setOnClickListener {
-         //   findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        ///
-
-       // }
     }
 }
+//private fun <T> LiveData<T>.observe(viewLifecycleOwner: LifecycleOwner, observer: Any, function: () -> Unit): T {
+
+//}
--- app/src/main/java/com/example/m4evaluacionintermedia/SecondFragment.kt
+++ app/src/main/java/com/example/m4evaluacionintermedia/SecondFragment.kt
@@ -1,17 +1,32 @@
 package com.example.m4evaluacionintermedia
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.m4evaluacionintermedia.DataBase.Nombre
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment() , NombreAdapter.PassTheData{
+
+    lateinit var viewModel: NombreViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NombreViewModel::class.java)
+    }
 
   override fun onCreateView(
       inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +39,29 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button).setOnClickListener {
+        val mRecyclerView = recyclerView
+
+        val mAdapter = NombreAdapter(this)
+        mRecyclerView.adapter = mAdapter
+        mRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        viewModel.allNombre.observe(viewLifecycleOwner, Observer {
+            Log.d("OBJETO", it.toString())
+
+            mAdapter.updateList(it)
+        })
+
+
+        fab.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+    }
+
+    override fun passTheData(mNombre: Nombre) {
+        val mBundle = Bundle()
+        mBundle.putInt("id", mNombre.id)
+        Toast.makeText(context, mNombre.id.toString(), Toast.LENGTH_LONG).show()
+        findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment, mBundle)
     }
   }
