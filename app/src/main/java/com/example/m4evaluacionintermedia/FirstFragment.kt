f9054be1ebe774425c3a1183ee34f36a11fd69f9
copito.123 <nicolspnicolsp@gmail.com>
2020-09-12 22:29:37 -0300

    YA NO FALTA TANTOOOOOOOOO

--- app/src/main/java/com/example/m4evaluacionintermedia/FirstFragment.kt
+++ app/src/main/java/com/example/m4evaluacionintermedia/FirstFragment.kt
@@ -6,11 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.example.m4evaluacionintermedia.DataBase.Nombre
 import kotlinx.android.synthetic.main.fragment_first.*
 
--- app/src/main/java/com/example/m4evaluacionintermedia/NombreAdapter.kt
+++ app/src/main/java/com/example/m4evaluacionintermedia/NombreAdapter.kt
@@ -1,4 +1,56 @@
 package com.example.m4evaluacionintermedia
 
-class NombreAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.example.m4evaluacionintermedia.DataBase.Nombre
+import kotlinx.android.synthetic.main.nombreitem_list.view.*
+
+class NombreAdapter(var mPasstheData: PassTheData) :RecyclerView.Adapter<NombreAdapter.NombreViewHolder>()  {
+
+    private var dataList = emptyList<Nombre>()
+
+    fun updateList(mDataList: List<Nombre>) {
+        dataList = mDataList
+        notifyDataSetChanged()
+
+    }
+
+    inner class NombreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+        val nombreText = itemView.idtv1
+        val nombreText2 = itemView.idtv2
+        val nombreText3 = itemView.idtv3
+        val chekNombre = itemView.cb2
+        val itemView = itemView.setOnClickListener(this)
+
+        override  fun onClick(p0: View?) {
+
+            mPasstheData.passTheData(dataList[adapterPosition])
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NombreViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.nombreitem_list,parent,false)
+        return NombreViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NombreViewHolder, position: Int) {
+       val mNombre: Nombre = dataList[position]
+        holder.nombreText.text = mNombre.nombre
+        holder.nombreText2.inputType = mNombre.cantidad.toInt()
+        holder.nombreText3.inputType = mNombre.precio.toInt()
+        holder.chekNombre.isChecked = mNombre.completeNombre
+    }
+
+    override fun getItemCount() = dataList.size
+
+   interface  PassTheData{
+       fun passTheData(mNombre: Nombre)
+    }
+
+
 }
